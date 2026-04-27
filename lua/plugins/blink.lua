9e4c05a52d31ef128af45f0faf0037c47fab795e
Hursit Peksen <peksen.h@brainrocket.com>
2026-04-27 13:23:58 +0100

    feat: add blink config

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,39 @@
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    cmdline = {
+      enabled = true,
+      keymap = {
+        preset = "super-tab",
+        ["<C-y>"] = { "select_and_accept" },
+        ["<Cr>"] = { "select_and_accept" },
+        ["<Right>"] = false,
+        ["<Left>"] = false,
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+        ["<M-space>"] = {
+          function(cmp)
+            cmp.show({ providers = { "snippets" } })
+          end,
+        },
+      },
+    },
+
+    keymap = {
+      preset = "super-tab",
+      ["<C-y>"] = { "select_and_accept" },
+      ["<Cr>"] = { "select_and_accept" },
+      ["<Right>"] = false,
+      ["<Left>"] = false,
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<M-space>"] = {
+        function(cmp)
+          cmp.show({ providers = { "snippets" } })
+        end,
+      },
+    },
+  },
+}
