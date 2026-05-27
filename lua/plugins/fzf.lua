e80dc6a4af8eeb44c1a265f259c5282e1af2325b
Hursit Peksen <peksen.h@brainrocket.com>
2026-05-27 10:33:12 +0100

    fix: update fzf grep

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,6 +1,5 @@
 return {
   "ibhagwan/fzf-lua",
-  cmd = "FzfLua",
   opts = function(_, opts)
     local fzf = require("fzf-lua")
     local actions = fzf.actions
@@ -14,6 +13,11 @@ return {
         },
       },
       grep = {
+        rg_glob = true,
+        glob_flag = "--iglob",
+        glob_separator = "%s%-%-",
+        resume = true,
+        cwd_prompt = true,
         actions = {
           ["ctrl-i"] = { actions.toggle_ignore },
           ["ctrl-h"] = { actions.toggle_hidden },
